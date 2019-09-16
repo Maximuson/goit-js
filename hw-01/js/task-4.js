a724c7f09405a676b312237a2519423a6f32bd86
Maximuson <maxpavlenko54@gmail.com>
2019-09-16 18:38:39 +0300

    fix hw-01 task 4

--- hw-01/js/task-4.js
+++ hw-01/js/task-4.js
@@ -1,17 +1,20 @@
 const credits = 23580;
 const priceDroid = 3000;
 let totalPrice = 0;
-let countBuyDroids = prompt('Введите количество');
+const countBuyDroids = prompt('Введите количество');
 
-countBuyDroids == null
-  ? (countBuyDroids = 'Отменено пользователем!')
-  : (totalPrice = priceDroid * countBuyDroids);
 
-if (totalPrice > credits) {
-  console.log('Недостаточно средств на счету!');
+if (countBuyDroids !== null) {
+  totalPrice = priceDroid * countBuyDroids;
+
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    console.log(
+      `Вы купили ${countBuyDroids} дроидов, на счету осталось ${credits
+                  - totalPrice} кредитов.'`,
+    );
+  }
 } else {
-  console.log(
-    `Вы купили ${countBuyDroids} дроидов, на счету осталось ${credits
-            - totalPrice} кредитов.'`,
-  );
+  console.log('Отменено пользователем!');
 }
