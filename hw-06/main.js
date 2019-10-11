e37ac426ca1cd5d7668789a5f48858cf05e33778
Maximuson <maxpavlenko54@gmail.com>
2019-10-11 20:33:28 +0300

    add destruct to arrow  functions

--- hw-06/main.js
+++ hw-06/main.js
@@ -1,56 +1,56 @@
 import users from './users.js';
 
-const getUserNames = (users) => users.map((user) => user.name);
+const getUserNames = (users) => users.map(({ name }) => name);
 
 console.log(getUserNames(users));
 
-const getUsersWithEyeColor = (users, color) => users.filter((user) => user.eyeColor === color);
+const getUsersWithEyeColor = (users, color) => users.filter(({ eyeColor }) => eyeColor === color);
 
-console.log(getUsersWithEyeColor(users, 'blue'));
+// console.log(getUsersWithEyeColor(users, 'blue'));
 
-const getUsersWithGender = (users, gender) => users.reduce((acc, e) => {
-  e.gender === gender && acc.push(e.name);
+const getUsersWithGender = (users, Inpgender) => users.reduce((acc, { name, gender }) => {
+  gender === Inpgender && acc.push(name);
   return acc;
 }, []);
 
 console.log(getUsersWithGender(users, 'male'));
 
-const getInactiveUsers = (users) => users.filter((e) => !e.isActive);
+const getInactiveUsers = (users) => users.filter(({ isActive }) => !isActive);
 
 console.log(getInactiveUsers(users));
 
-const getUserWithEmail = (users, email) => users.find((e) => e.email === email);
+const getUserWithEmail = (users, mail) => users.find(({ email }) => email === mail);
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
-const getUsersWithAge = (users, min, max) => users.filter((user) => user.age >= min && user.age <= max);
+const getUsersWithAge = (users, min, max) => users.filter(({ age }) => age >= min && age <= max);
 
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
 console.log(getUsersWithAge(users, 30, 40));
 
-const calculateTotalBalance = (users) => users.reduce((acc, e) => acc += e.balance,
+const calculateTotalBalance = (users) => users.reduce((acc, { balance }) => acc += balance,
   0);
 
 console.log(calculateTotalBalance(users));
 
-const getUsersWithFriend = (users, friendName) => users.filter((user) => user.friends.find((e) => e === friendName))
+const getUsersWithFriend = (users, friendName) => users.filter(({ friends }) => friends.find((e) => e === friendName))
   .map((user) => user.name);
 
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-const getNamesSortedByFriendsCount = (users) => users.sort((a, b) => a.friends.length - b.friends.length).map((e) => e.name);
+const getNamesSortedByFriendsCount = (users) => users.sort((a, b) => a.friends.length - b.friends.length).map(({ name }) => name);
 
 console.log(getNamesSortedByFriendsCount(users));
 
 const getSortedUniqueSkills = (users) => {
-  const skills = [];
-  users.forEach((e) => {
-    skills.push(...e.skills);
+  const skillsResult = [];
+  users.forEach(({ skills }) => {
+    skillsResult.push(...skills);
   });
-  return [...new Set(skills)].sort();
+  return [...new Set(skillsResult)].sort();
 };
 
 console.log(getSortedUniqueSkills(users));
